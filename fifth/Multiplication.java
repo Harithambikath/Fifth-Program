bd433ff71cbdbb8d5c5a9dd32fd3cc9defe0a5eb
Harithambika T H <harithambikat@gmail.com>
2022-07-27 11:20:14 +0530

    multiplication

---
+++ fifth/Multiplication.java
@@ -0,0 +1,14 @@
+class Multiplication
+{
+    public static void main(String args[])
+    {
+        int x=1;
+        System.out.println("The Multiplication table of 5 is");
+        do
+        {
+            System.out.println(x*5);
+            x++;
+        }
+        while(x<=10);
+    }
+}
